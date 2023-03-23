23853933cad87f3b5283b1d4d1d6cc4f33517917
ArtN0nymous <jr616934@gmail.com>
2023-03-23 08:30:43 -0700

    opts json

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,76 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-interface Componente {
-  icon:string,
-  name:string,
-  redirectTo:string
-}
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { MenuController } from '@ionic/angular';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  componentes :Componente[]=[{
-    icon:'american-football',
-    name:'Action Sheet',
-    redirectTo:'/action-sheet'
-  },{
-    icon:'alert-circle-outline',
-    name:'Alert',
-    redirectTo:'/alert'
-  },
-  {
-    icon:'person-circle-outline',
-    name:'Avatar',
-    redirectTo:'/avatar'
-  },{
-    icon:'radio-button-on-outline',
-    name:'Buttons',
-    redirectTo:'/buttons'
-  },{
-    icon:'id-card-outline',
-    name:'Cards',
-    redirectTo:'/cards'
-  },{
-    icon:'checkmark-circle-outline',
-    name:'Checkbox',
-    redirectTo:'/checkbox'
-  },{
-    icon:'calendar-outline',
-    name:'Datetime',
-    redirectTo:'/datetime'
-  },{
-    icon:'radio-button-off-outline',
-    name:'Fab',
-    redirectTo:'/fab'
-  },{
-    icon:'grid-outline',
-    name:'Grid',
-    redirectTo:'/grid'
-  },{
-    icon:'infinite-outline',
-    name:'Infinite Scroll',
-    redirectTo:'/infinite-scroll'
-  },{
-    icon:'text-outline',
-    name:'Inputs',
-    redirectTo:'/inputs'
-  },{
-    icon:'list-outline',
-    name:'List',
-    redirectTo:'/list'
-  },{
-    icon:'list',
-    name:'List reorder',
-    redirectTo:'/list-reorder'
-  },{
-    icon:'battery-charging-outline',
-    name:'Loading',
-    redirectTo:'/loading'
-  }];
-  constructor() { }
+  componentes !:Observable<Componente[]>;
+  constructor(private menuCtrl:MenuController,private datService:DataService) { }
 
   ngOnInit() {
+    this.componentes = this.datService.getMenuOpts();
   }
 
 }
